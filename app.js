3315547aee9c711ad0f2afe33687ea1dc86cbf0a
Adon Moskal <adonm@op.ac.nz>
2023-08-09 12:02:34 +1200

    ...

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ import express from 'express';
 
 // Import the index routes module
 import indexRoutes from './routes/index.js';
+import institutionRoutes from "./routes/institution.js";
+import departmentRoutes from "./routes/department.js";
 
 // Create an Express application
 const app = express();
 
+app.use(express.urlencoded({ extended: false })); // To parse the incoming requests with urlencoded payloads. For example, form data
+app.use(express.json()); // To parse the incoming requests with JSON payloads. For example, REST API requests
+
 // Use the routes module
 app.use('/', indexRoutes);
+app.use("/api/institutions", institutionRoutes);
+app.use("/api/departments", departmentRoutes);
+
 
 // Start the server on port 3000
 app.listen(3000, () => {
