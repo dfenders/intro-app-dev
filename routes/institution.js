5f1462564e0019873d328e388430b4a53b5207ea
Adon Moskal <adonm@op.ac.nz>
2023-08-16 11:54:47 +1200

    ...

--- routes/institution.js
+++ routes/institution.js
@@ -8,9 +8,11 @@ import {
   deleteInstitution,
 } from "../controllers/institution.js";
 
+import { validatePostInstitution } from "../middleware/validation.js";
+
 const router = express.Router();
 
-router.post("/", createInstitution);
+router.post("/", validatePostInstitution, createInstitution);
 router.get("/", getInstitutions);
 router.get("/:id", getInstitution);
 router.put("/:id", updateInstitution);
